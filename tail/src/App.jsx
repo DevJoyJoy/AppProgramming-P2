bee6f5d1450a51751bdcda66896b79cdb549cc9f
DevJoyJoy <joycenascimento2007@gmail.com>
2025-09-18 11:41:30 -0300

    New exercise

---
+++ tail/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div class="h-screen flex flex-col justify-between">
+      <div class="flex justify-between items-center">
+        <div class="w-52 h-52 flex flex-col bg-purple-950">
+          <div class="w-17 h-17 flex self-start bg-purple-300"></div>
+          <div class="w-17 h-17 flex self-end bg-purple-300"></div>
+        </div>
+        <div class="w-52 h-52 justify-center flex items-center bg-purple-950">
+          <div class="w-17 h-17 bg-purple-300"></div>
+        </div>
+        <div class="w-52 h-52 flex bg-purple-950">
+          <div class="w-17 h-17 flex self-start bg-purple-300"></div>
+          <div class="w-17 h-17 flex self-end bg-purple-300"></div>
+        </div>
+        <div class="w-52 h-52 justify-center items-end flex bg-purple-950">
+          <div class="w-17 h-17 bg-purple-300"></div>
+        </div>
+      </div>
+      <div class="flex justify-between items-center">
+        <div class="w-52 h-52 justify-center flex bg-purple-950">
+          <div class="w-17 h-17 bg-purple-300"></div>
+        </div>
+        <div class="w-52 h-52 flex flex-row bg-purple-950">
+          <div class="w-17 h-17 self-start bg-purple-300"></div>
+          <div class="w-17 h-17 self-end bg-purple-300"></div>
+          <div class="w-17 h-17 self-start bg-purple-300"></div>
+          <div class="w-17 h-17 self-end bg-purple-300"></div>
+        </div>
+        <div class="w-52 h-52 justify-center flex bg-purple-950">
+          <div class="w-17 h-17  bg-purple-300"></div>
+        </div>
+      </div>
+      <div class="flex justify-between items-center">
+        <div class="w-52 h-52 flex bg-purple-950">
+          <div class="w-17 h-17 flex self-end bg-purple-300"></div>
+          <div class="w-17 h-17 flex self-start bg-purple-300"></div>
+        </div>
+        <div class="w-52 h-52 justify-center flex items-center bg-purple-950">
+          <div class="w-17 h-17 bg-purple-300"></div>
+        </div>
+        <div class="w-52 h-52 flex justify-end bg-purple-950">
+          <div class="w-17 h-17 bg-purple-300"></div>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
