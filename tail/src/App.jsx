6d15f9413f92f0d9996ee8e0bb0eaf03f24d3f9f
DevJoyJoy <DevJoyJoy>
2025-09-24 09:01:56 -0300

    Finished the squares activity!!

--- tail/src/App.jsx
+++ tail/src/App.jsx
@@ -10,43 +10,54 @@ function App() {
     <>
     <div class="h-screen flex flex-col justify-between">
       <div class="flex justify-between items-center">
-        <div class="w-52 h-52 flex flex-col bg-purple-950">
-          <div class="w-17 h-17 flex self-start bg-purple-300"></div>
-          <div class="w-17 h-17 flex self-end bg-purple-300"></div>
+        <div class="w-52 h-52 flex justify-between flex-col items-center bg-purple-950">
+          <div class="w-17 h-17 flex bg-purple-300"></div>
+          <div class="w-17 h-17 flex bg-purple-300"></div>
         </div>
+        {/* ------------------------------------------------------------------------------ */}
         <div class="w-52 h-52 justify-center flex items-center bg-purple-950">
           <div class="w-17 h-17 bg-purple-300"></div>
         </div>
-        <div class="w-52 h-52 flex bg-purple-950">
-          <div class="w-17 h-17 flex self-start bg-purple-300"></div>
+        {/* ------------------------------------------------------------------------------ */}
+        <div class="w-52 h-52 flex justify-between bg-purple-950">
+          <div class="w-17 h-17 flex self-start justify-end bg-purple-300"></div>
           <div class="w-17 h-17 flex self-end bg-purple-300"></div>
         </div>
+        {/* ------------------------------------------------------------------------------ */}
         <div class="w-52 h-52 justify-center items-end flex bg-purple-950">
           <div class="w-17 h-17 bg-purple-300"></div>
         </div>
       </div>
+      {/* ------------------------------------------------------------------------------ */}
       <div class="flex justify-between items-center">
         <div class="w-52 h-52 justify-center flex bg-purple-950">
           <div class="w-17 h-17 bg-purple-300"></div>
+      </div>
+        {/* ------------------------------------------------------------------------------ */}
+        <div class="w-52 h-52 flex flex-col bg-purple-950">
+          <div class="w-17 h-17 flex self-center bg-purple-300"></div>
+          <div class="flex justify-between">
+            <div class="w-17 h-17 flex self-end bg-purple-300"></div>
+            <div class="w-17 h-17 flex self-start bg-purple-300"></div>
+          </div>
+          <div class="w-17 h-17 flex self-center justify-start bg-purple-300"></div>
         </div>
-        <div class="w-52 h-52 flex flex-row bg-purple-950">
-          <div class="w-17 h-17 self-start bg-purple-300"></div>
-          <div class="w-17 h-17 self-end bg-purple-300"></div>
-          <div class="w-17 h-17 self-start bg-purple-300"></div>
-          <div class="w-17 h-17 self-end bg-purple-300"></div>
-        </div>
+        {/* ------------------------------------------------------------------------------ */}
         <div class="w-52 h-52 justify-center flex bg-purple-950">
           <div class="w-17 h-17  bg-purple-300"></div>
         </div>
       </div>
+      {/* ------------------------------------------------------------------------------ */}
       <div class="flex justify-between items-center">
-        <div class="w-52 h-52 flex bg-purple-950">
-          <div class="w-17 h-17 flex self-end bg-purple-300"></div>
+        <div class="w-52 h-52 flex justify-between bg-purple-950">
+          <div class="w-17 h-17 flex self-end justify-end bg-purple-300"></div>
           <div class="w-17 h-17 flex self-start bg-purple-300"></div>
         </div>
+        {/* ------------------------------------------------------------------------------ */}
         <div class="w-52 h-52 justify-center flex items-center bg-purple-950">
           <div class="w-17 h-17 bg-purple-300"></div>
         </div>
+        {/* ------------------------------------------------------------------------------ */}
         <div class="w-52 h-52 flex justify-end bg-purple-950">
           <div class="w-17 h-17 bg-purple-300"></div>
         </div>
